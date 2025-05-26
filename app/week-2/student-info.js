ded1d8351455d16a6d52e4cd96ea9c5088663371
SidakMann <156955242+SidakMann@users.noreply.github.com>
2025-05-26 12:50:39 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Sidakveer</h1>
+      <p>
+        <Link href="https://github.com/SidakMann">
+          https://github.com/SidakMann
+        </Link>
+      </p>
+    </div>
+  );
+}
